5c7161fa08bc6956de6554e5ce3d72f0f332147b
Verti <a20220406@utem.edu.mx>
2023-11-27 17:41:41 -0600

    Eres un Gallo papá dale duro mi rey

--- IntroduccionPHP_inicio/10-isset-empty.php
+++ IntroduccionPHP_inicio/10-isset-empty.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes2 = array();
+$clientes3 = array('Pedro', 'Juan', 'Karen');
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200
+]; 
 
 
+// empty: Revisa si un arreglo esta vacio
+var_dump( empty($clientes) );
+echo "<br>"; 
+var_dump( empty($clientes2) );
+echo "<br>";
+var_dump( empty($clientes3) );
+echo "<br>";
 
+// isset: Revisa si un arreglo esta creado o una propiedad esta definida
+var_dump( isset($clientes) );
+var_dump( isset($clientes2) );
+var_dump( isset($clientes3) );
+var_dump( isset($clientes4) );
+
+var_dump(isset($cliente['nombre']));
 include 'includes/footer.php';
--- IntroduccionPHP_inicio/11-funciones-arrays.php
+++ IntroduccionPHP_inicio/11-funciones-arrays.php
@@ -1,7 +1,39 @@
 <?php include 'includes/header.php';
 
+// in_array: buscar elementos
+$carrito = ['tablet', 'television', 'computadora'];
+var_dump( in_array('tablet', $carrito) );
+echo "<br>";
+var_dump(in_array('iphone', $carrito)); 
 
+// ordenar elementos de un array
+$numeros = array(3,4,5,1,2);
+sort($numeros); // ordenar de menor a mayor
 
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+//de mayor a menor
+rsort($numeros);
+
+// ordenar arreglo asociativo
+$cliente = array(
+    'saldo' => 200,
+    'tipo' => 'premium',
+    'nombre' => 'juan'
+);
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+echo "<pre>";
+asort($cliente); // ordenar por valor 
+arsort($cliente); // ordenar por valor 
+ksort($cliente); // ordenar por clave orden alfabético
+krsort($cliente); // ordenar por clave en orden inverso alfabético
+echo "</pre>";
 
 
 include 'includes/footer.php';
