3df526ed83b4274622909960919b355b6a83c536
Verti <a20220406@utem.edu.mx>
2023-11-29 08:35:00 -0600

    cambios a todo

--- IntroduccionPHP_inicio/12-condicionales.php
+++ IntroduccionPHP_inicio/12-condicionales.php
@@ -1,7 +1,67 @@
 <?php include 'includes/header.php';
 
+$autenticado = false;
+$admin = true;
+
+if($autenticado && $admin) {
+    echo "El usuario esta autenticado";
+}else {
+    echo "El usuario no esta autenticado, Inicia sesion";
+    echo "<br/>";
+}
+
+//if anidados
+
+$cliente = [
+    'nombre' => 'Alondra <3',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ]
+];
+
+if(!empty($cliente)) {
+    echo "El arreglo de cliente no esta vacio";
+    echo "<br/>";
+    echo ($cliente['nombre']);
+    echo "<br/>";
+
+    if($cliente['saldo'] > 0) {
+        echo "El cliente tiene saldo disponible";
+        echo "<br/>";
+    } else {
+        echo "No hay saldo";
+        echo "<br/>";
+    }
+} 
 
 
+//else if 
+
+if($cliente['saldo'] > 201) {
+    echo "El cliente tiene saldo disponible";
+} else if ($cliente['informacion']['tipo'] === 'premium') {
+    echo "El cliente es premium";
+    echo "<br/>";   
+} else {
+    echo "No hay limite de saldo";
+    echo "<br/>";   
+}
+
+
+//switch
+
+$tecnologia = 'php';
+
+switch($tecnologia) {
+    case 'php':
+        echo "PHP, un excelente lenguaje";
+        break;
+
+        default:
+            echo "Aun no hay contenido para esta tecnologia";
+            break;
+} 
 
 
 include 'includes/footer.php';
--- IntroduccionPHP_inicio/13-loops.php
+++ IntroduccionPHP_inicio/13-loops.php
@@ -1,6 +1,74 @@
 <?php include 'includes/header.php';
 
+//!! loops
+
+//while 
+$i = 0; //inicializador
+
+while ($i < 10) {
+
+    echo $i . "<br/>";
+
+    $i++; //incremento
+
+}
+
+echo "<br/>";
 
 
+//do while
+$i = 0;
+
+do   {
+        echo $i . "<br/>";
+        $i++;
+    } while ($i < 10); {
+}
+
+
+echo "<br/>";
+
+for ($i = 0; $i < 100; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo $i . "-FizzBuzz <br/>";
+    } else if ($i % 3 === 0) {
+        echo $i . "-Fizz <br/>";
+    } else if ($i % 5 === 0) {
+        echo $i . "-Buzz <br/>";
+    } else {
+        echo $i . "<br/>";
+    }
+}
+
+echo "<br/>";
+
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach ($clientes as $cliente) {
+    echo $cliente . "<br/>";
+}
+
+echo count ($clientes);
+
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach ($cliente as $key => $valor) {
+    echo $key . " - " . $valor . "<br/>";
+}
+
+
+$hola = [
+    'nombre' => 'Carlos',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach ($hola as $llave => $valor ) {
+    echo $llave . "-" . $valor . "<br/>";
+}
 
 include 'includes/footer.php';
